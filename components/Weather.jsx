55da9ba732afa747e00086d4063dfec485c3bdda
Bacteria007 <123715803+Bacteria007@users.noreply.github.com>
2024-07-17 17:19:28 +0500

    app launch

---
+++ components/Weather.jsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import React from "react";
+
+const kelvinToCelsius = (kelvin) => {
+    return kelvin - 273.15;
+  };
+
+const Weather = ({ data }) => {
+  console.log(data);
+
+  return (
+    <div className="relative flex flex-col justify-between max-w-[500px] w-full h-[90vh] m-auto p-4 text-gray-300 z-10">
+      {/* Top */}
+      <div className="relative flex justify-between pt-12">
+        <div className="flex flex-col items-center">
+          <Image
+            width={100}
+            height={100}
+            alt="/"
+            src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+          />
+          <p className="text-2xl">{data.weather[0].main}</p>
+        </div>
+        <p className="text-9xl">{kelvinToCelsius(data.main.temp).toFixed(0)}&#176;C</p>
+      </div>
+      {/* bottom */}
+      <div className="bg-black/50 relative p-8 rounded-md">
+        <p className="text-2xl text-center pb-6">Weather {data.name}</p>
+        <div className="flex justify-between text-center">
+          <div>
+            <p className="text-2xl font-bold"> {kelvinToCelsius(data.main.feels_like).toFixed(0)}&#176;C</p>
+            <p className="text-xl">Feels Like</p>
+          </div>
+          <div>
+            <p className="text-2xl  font-bold"> {data.main.humidity}% </p>
+            <p className="text-xl">Humidity</p>
+          </div>
+          <div>
+            <p className="text-2xl font-bold"> {data.wind.speed.toFixed(0)} MPH</p>
+            <p className="text-xl">Winds</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,39 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// module.exports = {
+//   images: {
+//     domains: [
+//       'images.unsplash.com', 'openweathermap.org'
+//     ]
+//   }
+// }
+
+
+
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: "http",
+        hostname: 'openweathermap.org',
+        port: '',
+        pathname: '/img/wn/**',
+      }
+
+    ]
+  }
+};
 
 export default nextConfig;
